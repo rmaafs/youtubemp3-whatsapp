72ca2e734da3d5319438f097e5d80b41557ffaa6
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-07-30 22:04:16 -0500

    Se arregló comando para test con jest

--- src/__test__/estandar.test.js
+++ src/__test__/estandar.test.js
@@ -1,7 +1,7 @@
 var checkFilesExist = require("check-files-exist");
 
 describe("Verificar estándar de código (únicamente uso de .js)", () => {
-  test("Que no existan archivos .js para código", async () => {
+  test("Que no existan archivos .jsx para código", async () => {
     await checkFilesExist(["src/Main/**/*.jsx"]).then(
       (data) => {
         //Por favor, usa únicamente archivos .js para código.
